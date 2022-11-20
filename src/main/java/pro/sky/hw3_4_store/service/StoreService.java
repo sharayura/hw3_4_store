8a5f9f1e42bcabc7029f66ba17f3e323950ddea8
sharayura <sharapyura@gmail.com>
2022-11-20 21:32:23 +0300

    hw3.4

--- src/main/java/pro/sky/hw3_4_store/service/StoreService.java
+++ src/main/java/pro/sky/hw3_4_store/service/StoreService.java
@@ -1,2 +1,22 @@
-package pro.sky.hw3_4_store.service;public class StoreService {
+package pro.sky.hw3_4_store.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.SessionScope;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@SessionScope
+public class StoreService {
+
+    private final List<Integer> ids = new ArrayList<>();
+
+    public List<Integer> getIds() {
+        return ids;
+    }
+
+    public void addIds(List<Integer> idsToAdd) {
+        ids.addAll(idsToAdd);
+    }
 }
